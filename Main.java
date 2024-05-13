90efea664578205f4b16619592dd683adacb112c
Lucas Menon <lucasguilarduccimenon@gmail.com>
2024-05-13 17:35:31 -0300

    :arrow_down: deletando classes fora do bluej

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    try {
-      MenuPrincipal.apresentacao();
-    } catch (Exception e) {
-      System.out.println("Tomou uma exeção de: " + e.getMessage());
-    }
-  }
-}

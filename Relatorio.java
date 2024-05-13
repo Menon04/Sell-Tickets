2a4b28f28cbb0ff3d041b01442a29cd8b6556280
Lucas Menon <lucasguilarduccimenon@gmail.com>
2024-05-13 17:20:38 -0300

    :recycle: pequenas alteracoes na String relatorio

--- Relatorio.java
+++ Relatorio.java
@@ -4,16 +4,18 @@ public class Relatorio {
   private static ArrayList<Evento> eventos = CadastradorEventos.eventos;
 
   public static String relatorioEventos(){
-    String relatorio = "";
+    StringBuilder relatorio = new StringBuilder();
     if (eventos.isEmpty()) {
       return "Não há eventos cadastrados!";
     }
 
     for (Evento evento : eventos) {
-      relatorio += evento.getNome() + "\n" 
-      + "Arrecadou um total de: R$" + evento.getTotalArrecadado() + "\n" 
-      + "Teve um total de: " + evento.getCapacidade() + " asssentos ocupados" + "\n";
+      relatorio.append(evento.getNome()).append("\n");
+      relatorio.append("Arrecadou um total de: R$")
+               .append(String.format("%.2f", evento.getTotalArrecadado())).append("\n");
+      relatorio.append("Teve um total de: ").append(1751 - evento.getCapacidade())
+               .append(" assentos ocupados\n");
     }
-    return relatorio;
+    return relatorio.toString();
   }
 }
